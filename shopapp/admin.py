605a62a0f5cb9e75bd2ae9c5c92596242e076964
kiemluc01 <kiemluc01@gmail.com>
2022-12-15 14:59:29 +0700

    admin register model

--- shopapp/admin.py
+++ shopapp/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Userprofile, Department, CreditCard, ProductCategory, Product, DetailProduct, Comment, Image, Orderring, BillOrder
 
 # Register your models here.
+admin.site.register(Userprofile)
+admin.site.register(Department)
+admin.site.register(CreditCard)
+admin.site.register(ProductCategory)
+admin.site.register(Product)
+admin.site.register(DetailProduct)
+admin.site.register(Comment)
+admin.site.register(Image)
+admin.site.register(Orderring)
+admin.site.register(BillOrder)
