aa6882f7ff8304186c6fcd88e1d2deac53003631
kiemluc01 <kiemluc01@gmail.com>
2023-04-13 12:02:46 +0700

    checklogs

--- shopapp/views.py
+++ shopapp/views.py
@@ -33,7 +33,7 @@ class ProductViewset(viewsets.ModelViewSet):
     @action(detail=True, methods=['GET'])
     def get_image(self, request, pk=None):
         images = Image.objects.filter(product=pk)
-        print(images)
+        print(ImageSerializer(data=images).data)
         return Response(ImageSerializer(images).data, status=status.HTTP_200_OK)
     
 class CommentViewset(viewsets.ModelViewSet):
